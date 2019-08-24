6406a107e3b948d9e4c036d233c98b90b557283b
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2019-08-24 17:15:49 +0800

    First commit

---
+++ app/index.js
@@ -0,0 +1,33 @@
+import './styles/global.scss'
+
+import React, { Suspense } from 'react'
+import { hot } from 'react-hot-loader'
+import ReactDOM from 'react-dom'
+
+import RouteWithSubroutes from 'Components/RouteWithSubRoutes'
+import routes from './routes'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+const env = process.env.NODE_ENV
+
+const App = () => {
+  return (
+    <Router>
+      <Suspense fallback={<p>Loading</p>}>
+        <Switch>
+          {routes.map((route, i) => <RouteWithSubroutes key={i} {...route} />)}
+        </Switch>
+      </Suspense>
+    </Router>
+  )
+}
+
+const $app = document.getElementById('app')
+if (env === 'development') {
+  console.log(`Console Environment is ${env}`)
+
+  const HotApp = hot(module)(App)
+  ReactDOM.render(<HotApp />, $app)
+} else {
+  ReactDOM.render(<App />, $app)
+}
