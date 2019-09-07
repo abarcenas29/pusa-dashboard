4e60bbd828dc84b061604e21fb2d35e9649af259
Aldrich Allen Barcenas <aldrich.barcenas@gmail.com>
2019-09-07 19:38:43 +0800

    Add FakeLogin function and fake local users

--- app/containers/Login/index.js
+++ app/containers/Login/index.js
@@ -1,33 +1,113 @@
-import React from 'react'
-import { Button, Form, Grid, Header, Message, Segment } from 'semantic-ui-react'
+import 'react-toastify/dist/ReactToastify.css'
+
+import React, { useEffect } from 'react'
+import {
+  Button,
+  Form,
+  Grid,
+  Header,
+  Message,
+  Segment
+} from 'semantic-ui-react'
+import { ToastContainer, toast } from 'react-toastify'
+import { Form as ReactForm, Field } from 'react-final-form'
+
+import Users from './fakeLogin'
 
 export default ({ history }) => {
+  const onSubmit = value => {
+    const { email, password } = value
+    const foundUsers = Users.filter(user => (
+      user.email === email &&
+      user.password === password
+    ))
+
+    if (foundUsers.length > 0) {
+      const {
+        email,
+        firstName,
+        id,
+        lastName,
+        orgId,
+        role
+      } = foundUsers[0]
+
+      localStorage.setItem('email', email)
+      localStorage.setItem('firstName', firstName)
+      localStorage.setItem('lastName', lastName)
+      localStorage.setItem('id', id)
+      localStorage.setItem('orgId', orgId)
+      localStorage.setItem('role', role)
+      history.push('/dashboard')
+
+      toast.success('Logged In')
+    } else {
+      toast.error('Invalid Username/Password')
+    }
+  }
+
+  useEffect(() => {
+    localStorage.clear()
+  }, [])
+
   return (
     <Grid textAlign='center' style={{ height: '100vh' }} verticalAlign='middle'>
-      <Grid.Column style={{ maxWidth: 500 }}>
+      <Grid.Column style={{ maxWidth: 500, minWidth: 500 }}>
         <Header as='h2' color='teal' textAlign='center'>
           Pop-Store Attendance System & Payroll
         </Header>
-        <Form size='large'>
-          <Segment stacked>
-            <Form.Input fluid icon='user' iconPosition='left' placeholder='E-mail address' />
-            <Form.Input
-              fluid
-              icon='lock'
-              iconPosition='left'
-              placeholder='Password'
-              type='password'
-            />
+        <ReactForm
+          onSubmit={onSubmit}
+        >
+          {
+            ({ handleSubmit }) => (
+              <Form size='large' onSubmit={handleSubmit}>
+                <Segment stacked>
+                  <Field name='email'>
+                    {
+                      props => (
+                        <Form.Input
+                          fluid
+                          icon='user'
+                          iconPosition='left'
+                          placeholder='E-mail address'
+                          {...props.input}
+                        />
+                      )
+                    }
+                  </Field>
+                  <Field name='password' type='password'>
+                    {
+                      props => (
+                        <Form.Input
+                          fluid
+                          icon='lock'
+                          iconPosition='left'
+                          placeholder='Password'
+                          {...props.input}
+                        />
+                      )
+                    }
+                  </Field>
 
-            <Button color='teal' fluid size='large' onClick={() => history.push('/dashboard')}>
-            Login
-            </Button>
-          </Segment>
-        </Form>
+                  <Button
+                    type='submit'
+                    color='teal'
+                    fluid
+                    size='large'
+                  >
+                    Login
+                  </Button>
+                </Segment>
+              </Form>
+            )
+          }
+        </ReactForm>
         <Message>
           New to us? <a href='#'>Sign Up</a>
         </Message>
       </Grid.Column>
+      <ToastContainer />
     </Grid>
   )
 }
